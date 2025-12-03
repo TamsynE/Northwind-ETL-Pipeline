717fb2810082205a6708a56c1e49a6d6739cf1b9
Tamsyn Evezard <tamsyn.evezard@eagles.cui.edu>
2025-12-02 20:08:03 -0500

    Update queries.sql

--- analytics/queries.sql
+++ analytics/queries.sql
@@ -1,22 +1,22 @@
 -- Monthly Revenue
 SELECT
-  EXTRACT(YEAR FROM orderdate) AS year,
-  EXTRACT(MONTH FROM orderdate) AS month,
-  SUM(revenue) AS total_revenue
+  YEAR(orderDate) AS year,
+  MONTH(orderDate) AS month,
+  ROUND(SUM(revenue),2) AS total_revenue
 FROM fact_sales
 GROUP BY year, month
 ORDER BY year, month;
 
 -- Best-Selling Products
 SELECT
-  p.productname,
+  p.productName,
   SUM(f.quantity) AS total_units_sold,
-  SUM(f.revenue) AS total_revenue
+  ROUND(SUM(f.revenue),2) AS total_revenue
 FROM fact_sales f
 JOIN dim_products p
   ON f.productid = p.productid
-GROUP BY p.productname
-ORDER BY total_units_sold DESC
+GROUP BY p.productName
+ORDER BY total_units_sold DESC, total_revenue DESC
 LIMIT 10;
 
 -- Category Performance
@@ -32,24 +32,24 @@ ORDER BY revenue DESC;
 
 -- Top Customers
 SELECT
-  c.customerid,
-  c.companyname,
+  c.customerID as customer_id,
+  c.companyName as company,
   SUM(f.revenue) AS total_revenue,
-  COUNT(DISTINCT f.orderid) AS orders
-FROM fact_sales f
+  COUNT(DISTINCT f.orderid) AS num_orders
+FROM fact_orders f
 JOIN dim_customers c
-  ON f.customerid = c.customerid
-GROUP BY c.customerid, c.companyname
+  ON f.customerID = c.customerID
+GROUP BY c.customerID, c.companyName
 ORDER BY total_revenue DESC
 LIMIT 10;
 
 -- Shipping Revenue by Country
 SELECT
   c.country,
-  SUM(f.revenue) AS revenue
+  ROUND(SUM(f.revenue),2) AS revenue
 FROM fact_sales f
 JOIN dim_customers c
-  ON f.customerid = c.customerid
+  ON f.customerID = c.customerID
 GROUP BY c.country
 ORDER BY revenue DESC;
 
@@ -75,11 +75,11 @@ ORDER BY year, month, revenue DESC;
 
 -- Top Sales per Employee
 SELECT
-  e.firstname || ' ' || e.lastname AS employee,
+  CONCAT(e.firstname, ' ', e.lastname) AS employee,
   SUM(f.revenue) AS revenue
 FROM fact_sales f
 JOIN dim_employees e
-  ON f.employeeid = e.employeeid
+  ON f.employeeID = e.employeeID
 GROUP BY employee
 ORDER BY revenue DESC;
 
