22d20672702592bf2cb832275d59f006898d6206
Tamsyn Evezard <93171379+TamsynE@users.noreply.github.com>
2025-12-06 15:21:40 -0500

    Update queries.sql

--- analytics/queries.sql
+++ analytics/queries.sql
@@ -80,3 +80,99 @@ JOIN `etl-project-478115.Northwind.dim_employees` e
   ON f.employeeID = e.employeeID
 GROUP BY employee, title
 ORDER BY revenue DESC;
+
+-- Average Daily Revenue
+WITH daily_revenue AS (
+  SELECT
+    EXTRACT(YEAR FROM orderDate) AS year,
+    EXTRACT(MONTH FROM orderDate) AS month,
+    EXTRACT(DAY FROM orderDate) AS day,
+    SUM(revenue) AS total_revenue
+  FROM `etl-project-478115.Northwind.fact_orders`
+  GROUP BY year, month, day
+)
+SELECT
+  ROUND(AVG(total_revenue), 2) AS avg_daily_revenue
+FROM daily_revenue;
+
+-- Average Monthly Revenue
+WITH monthly_revenue AS (
+  SELECT
+    EXTRACT(YEAR FROM orderDate) AS year,
+    EXTRACT(MONTH FROM orderDate) AS month,
+    SUM(revenue) AS total_revenue
+  FROM `etl-project-478115.Northwind.fact_orders`
+  GROUP BY year, month
+)
+SELECT
+  ROUND(AVG(total_revenue), 2) AS avg_monthly_revenue
+FROM monthly_revenue;
+
+-- Average Weekly Revenue
+WITH weekly_revenue AS (
+  SELECT
+    DATE_TRUNC(orderdate, WEEK) AS week_start,
+    SUM(revenue) AS total_revenue
+  FROM `etl-project-478115.Northwind.fact_orders`
+  GROUP BY week_start
+)
+SELECT
+  ROUND(AVG(total_revenue), 2) AS avg_weekly_revenue
+FROM weekly_revenue;
+
+-- Max, Min, Average Daily Revenue
+(SELECT
+  EXTRACT(YEAR FROM orderDate) AS year,
+  EXTRACT(MONTH FROM orderDate) AS month,
+  EXTRACT(DAY FROM orderDate) AS day,
+  ROUND(SUM(revenue),2) AS total_revenue
+FROM `etl-project-478115.Northwind.fact_orders` 
+GROUP BY year, month, day
+ORDER BY total_revenue
+LIMIT 1)
+UNION ALL
+(SELECT
+  EXTRACT(YEAR FROM orderDate) AS year,
+  EXTRACT(MONTH FROM orderDate) AS month,
+  EXTRACT(DAY FROM orderDate) AS day,
+  ROUND(SUM(revenue),2) AS total_revenue
+FROM `etl-project-478115.Northwind.fact_orders` 
+GROUP BY year, month, day
+ORDER BY total_revenue DESC
+LIMIT 1)
+
+-- Max, Min, Average Monthly Revenue
+(SELECT
+  EXTRACT(YEAR FROM orderDate) AS year,
+  EXTRACT(MONTH FROM orderDate) AS month,
+  ROUND(SUM(revenue),2) AS total_revenue
+FROM `etl-project-478115.Northwind.fact_orders` 
+GROUP BY year, month
+ORDER BY total_revenue
+LIMIT 1)
+UNION ALL
+(SELECT
+  EXTRACT(YEAR FROM orderDate) AS year,
+  EXTRACT(MONTH FROM orderDate) AS month,
+  ROUND(SUM(revenue),2) AS total_revenue
+FROM `etl-project-478115.Northwind.fact_orders` 
+GROUP BY year, month
+ORDER BY total_revenue DESC
+LIMIT 1)
+
+-- Max, Min, Average Weekly Revenue
+(SELECT
+  DATE_TRUNC(orderdate, WEEK) AS week_start,
+  ROUND(SUM(revenue), 2) AS total_revenue
+FROM `etl-project-478115.Northwind.fact_orders`
+GROUP BY week_start
+ORDER BY total_revenue DESC
+LIMIT 1)
+UNION ALL
+(SELECT
+  DATE_TRUNC(orderdate, WEEK) AS week_start,
+  ROUND(SUM(revenue), 2) AS total_revenue
+FROM `etl-project-478115.Northwind.fact_orders`
+GROUP BY week_start
+ORDER BY total_revenue ASC
+LIMIT 1);
